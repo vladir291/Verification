665f378673190aae3775900b8fba7da7c5d18a88
FranciscoUS <franciscohidalgoplaza@gmail.com>
2015-12-14 13:13:29 +0100

    Añadido metodos de RSA (con pequeñas modificaciones para hacerlos independientes del código antiguo) a la clase auxiliar, la cual implementará todos los metodos a partir de ahora.

--- verificacion/src/main/AuxClass.java
+++ verificacion/src/main/AuxClass.java
@@ -3,8 +3,18 @@ package main;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
 import javax.crypto.BadPaddingException;
@@ -14,6 +24,7 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
 
 public class AuxClass {
 	
@@ -110,4 +121,116 @@ public class AuxClass {
 		return textodesencriptado;
 	}
 	
+	//cifrado RSA
+	public byte[] encrypt(String idVote,String textToEncypt){
+		
+		byte[] res = null;
+		try {
+			Cipher rsa;
+			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+			KeySpec keySpec = new X509EncodedKeySpec(DatatypeConverter.parseBase64Binary(getPublicKeyRSA(idVote)));
+			
+			PublicKey pubKeyFromBytes = keyFactory.generatePublic(keySpec);
+			
+			rsa = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+			rsa.init(Cipher.ENCRYPT_MODE, pubKeyFromBytes);
+	    
+		
+			res = rsa.doFinal(textToEncypt.getBytes());
+		} catch (IllegalBlockSizeException | BadPaddingException | InvalidKeyException | InvalidKeySpecException | NoSuchAlgorithmException | NoSuchPaddingException e) {
+			
+			e.printStackTrace();
+		} 
+		
+		return res;
+	}
+	
+	//descifrado RSA
+	public String decrypt(String idVote,byte[] cipherText) throws BadPaddingException{
+		
+		String res = null;
+		try {
+			Cipher rsa;
+			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+			KeySpec keySpec = new PKCS8EncodedKeySpec(DatatypeConverter.parseBase64Binary(getPrivateKeyRSA(idVote)));
+			
+			PrivateKey privKeyFromBytes = keyFactory.generatePrivate(keySpec);
+			
+			rsa = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+			rsa.init(Cipher.DECRYPT_MODE, privKeyFromBytes);
+			
+	    
+		
+			byte[] bytesDesencriptados = rsa.doFinal(cipherText);
+		    res = new String(bytesDesencriptados);
+		} catch (IllegalBlockSizeException  | InvalidKeyException | InvalidKeySpecException | NoSuchAlgorithmException | NoSuchPaddingException e) {
+			
+			e.printStackTrace();
+		}
+
+		
+		return res;
+	}
+	
+	//metodos auxiliares (RSA):
+	public boolean postKeyRSA(String id) {
+		boolean success = false;
+		try {
+
+			
+			 KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+			
+			 SecureRandom random = SecureRandom.getInstance("SHA1PRNG","SUN");
+			 keyGen.initialize(2048, random);
+			
+			 KeyPair pair = keyGen.generateKeyPair();
+			
+			 
+			 String publicKey = DatatypeConverter.printBase64Binary(pair.getPublic().getEncoded());
+			 String privateKey= DatatypeConverter.printBase64Binary(pair.getPrivate().getEncoded());
+			 
+			 
+			 RemoteDataBaseManager rdbm=new RemoteDataBaseManager();
+			 if (rdbm.postKeys(id, publicKey, privateKey)){
+				 success = true;
+			 }
+			
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	
+		
+		return success;
+	}
+
+	
+	public String getPublicKeyRSA(String id) {
+		RemoteDataBaseManager rdbm=new RemoteDataBaseManager();
+		
+		return rdbm.getPublicKey(id);
+	}
+
+	
+	public String getPrivateKeyRSA(String id) {
+
+		RemoteDataBaseManager rdbm=new RemoteDataBaseManager();
+		
+		
+		return rdbm.getPrivateKey(id);
+	}
+
+	
+	public boolean checkVoteRSA(byte[] votoCifrado, String id) {
+		
+		boolean res = true;
+		try {
+			decrypt(id, votoCifrado);
+		} catch (BadPaddingException e) {
+			res = false;
+		}
+		return res;
+
+	}
 }
