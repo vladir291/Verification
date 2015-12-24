e6e9d5ca0fc54273eac760c0eefd53a8725fdc06
vladir291 <vladir291@gmail.com>
2015-12-24 20:36:20 +0100

    Creado el método que devuelve las claves públicas de la votación y modificados los metodos de encriptar y desencriptar.

--- Verification/src/main/java/EGC/Verification/AuxClass2.java
+++ Verification/src/main/java/EGC/Verification/AuxClass2.java
@@ -3,14 +3,18 @@ package EGC.Verification;
 public class AuxClass2 {
 	
 	public static void metodoPrueba1(){
-		
+		//cambio
 	}
 	
 	public static void metodoPrueba2(){
-		
+		//cambio
 	}
 	
 	public static void metodoPrueba3(){
+		
+	}
+	
+	public static void metodoPrueba4(){
 		
 	}
 
